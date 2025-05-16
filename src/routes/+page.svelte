24b242aed17a417b192dc45d379e6652e7e47e80
XIYO <bunny@xiyo.dev>
2025-05-16 15:25:34 +0900

    :art: 분기 메뉴 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
-<h1><a href="yeon">TODOs App</a></h1>
+<ul>
+	<li><a href="yeon">Yeon's TODOs App</a></li>
+	<li><a href="heee">Heee's TODOs App</a></li>
+	<li><a href="hell">hell's TODOs App</a></li>
+	<li><a href="lim">Lim's TODOs App</a></li>
+</ul>
