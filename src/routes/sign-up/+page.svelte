abe09c4c5c5365337b480331df5494f51a547693
XIYO <bunny@xiyo.dev>
2025-05-19 18:33:42 +0900

    :truck: 이름 변경

---
+++ src/routes/sign-up/+page.svelte
@@ -0,0 +1,18 @@
+<form>
+	<h1>회원가입</h1>
+
+	<label>이메일 <input id="email" required type="email" /></label>
+
+	<label>비밀번호 <input id="password" required type="password" />
+	</label>
+
+	<label>비밀번호 확인 <input id="confirm-password" required type="password" />
+	</label>
+
+	<button type="submit">회원가입</button>
+</form>
+
+<p>
+	이미 계정이 있으신가요?
+	<a href="/sign-in">사인-인</a>
+</p>
