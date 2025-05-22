83be7e3e5989ba09f4c2b35229af6b058b437e32
XIYO <bunny@xiyo.dev>
2025-05-22 10:41:19 +0900

    feat: add updateTodo function and enhance todo management UI

--- src/lib/todos.js
+++ src/lib/todos.js
@@ -5,7 +5,9 @@ export const getTodo = (id) => {
 export const createTodo = ({title, description}) => {
 	return {url:`/api/todos`, options: { method: 'POST', body: JSON.stringify({title, description}), headers: { 'Content-Type': 'application/json' } } };
 }
+export const updateTodo = (id, {done}) => {
+	return {url:`/api/todos/${id}`, options: { method: 'PATCH', body: JSON.stringify({done}), headers: { 'Content-Type': 'application/json' } } };
+}
 export const deleteTodo = (id) => {
 	return {url:`/api/todos/${id}`, options: { method: 'DELETE' } };
 }
-
