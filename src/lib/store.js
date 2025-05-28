d4a975decb9b4715d902a5ee0ece7533c1ad10e8
XIYO <bunny@xiyo.dev>
2025-05-29 00:25:29 +0900

    :coffin: 사용하지 않는 코드 삭제

--- src/lib/store.js
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const todos = writable([
-	{ id: 1, title: "Demo 1", description: "설명입니다", done: false },
-	{ id: 2, title: "Demo 2", description: "완료된 작업입니다", done: true }
-]);
