8c6a672c2574afc06decd3b6cabc12750102469d
XIYO <bunny@xiyo.dev>
2025-06-07 03:23:53 +0900

    :recycle: 사용자 정보를 로컬에서 가져오도록 수정 및 리다이렉트 추가

--- src/routes/(main)/members/[nickname]/todos/+page.server.js
+++ src/routes/(main)/members/[nickname]/todos/+page.server.js
@@ -1,31 +1,32 @@
-import { fail } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 
 export const actions = {
-	create: async ({ request, locals : { zzic } }) => {
-		const userPromise = zzic.auth.getUser();
+	create: async ({ request, locals : { zzic, user }, url }) => {
 		const formData = await request.formData();
 		const title = formData.get('title');
 		const description = formData.get('description');
-		const user = (await userPromise).data.user;
-		const { error } = await zzic.todo.createTodo(user.sub, { title, description });
+		const { error } = await zzic.todo.createTodo({ userId: user.sub }, { title, description });
 		if (error) {
 			return fail(400, { error: error.message || '할 일 생성 실패' });
 		}
+		throw redirect(303, url.pathname);
 	},
-	done: async ({ request, locals : { zzic, user } }) => {
+	done: async ({ request, locals : { zzic, user }, url }) => {
 		const formData = await request.formData();
 		const id = formData.get('id');
 		const { error } = await zzic.todo.updateTodo({ userId: user.sub, todoId: id}, { done: true });
 		if (error) {
 			return fail(400, { error: error.message || '완료 처리 실패' });
 		}
+		throw redirect(303, url.pathname);
 	},
-	undone: async ({ request, locals : { zzic, user } }) => {
+	undone: async ({ request, locals : { zzic, user }, url }) => {
 		const formData = await request.formData();
 		const id = formData.get('id');
 		const { error } = await zzic.todo.updateTodo({ userId: user.sub, todoId: id}, { done: false });
 		if (error) {
 			return fail(400, { error: error.message || '완료 처리 실패' });
 		}
+		throw redirect(303, url.pathname);
 	}
 };
