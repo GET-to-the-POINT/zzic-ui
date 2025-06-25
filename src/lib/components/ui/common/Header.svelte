b7a9e7d5797b9ade1e09c441ff0c562d2e421064
xiyo <bunny@xiyo.dev>
2025-06-25 23:50:54 +0900

    fix(Header.svelte): 링크 클릭 시 뒤로 가기 기능 추가 및 버튼에서 앵커 태그로 변경

--- src/lib/components/ui/common/Header.svelte
+++ src/lib/components/ui/common/Header.svelte
@@ -3,13 +3,18 @@
 	import ChevronLeft from '@lucide/svelte/icons/chevron-left';
 
     import { page } from '$app/state';
+
+    const handleBack = (e) => {
+        e.preventDefault();
+        window.history.back();
+    };
 </script>
 
 <header class="preset-filled-primary-500 p-4 h-12 flex items-center justify-between">
-    <button class="btn-icon w-6 h-6">
+    <a href="./" class="btn-icon w-6 h-6" onclick={handleBack}>
         <ChevronLeft class="w-6 h-6" />
-    </button>
-    <h1 class="te`xt-2xl font-semibold uppercase">{page.data.meta.title ?? 'header'}</h1>
+    </a>
+    <h1 class="text-2xl font-semibold uppercase">{page.data.meta?.title ?? 'header'}</h1>
     <button class="btn-icon w-6 h-6">
         <Bell class="w-6 h-6" />
     </button>
