cee57fdfce7112ccb59fe7b502533f922d990e1d
xiyo <bunny@xiyo.dev>
2025-06-26 11:13:03 +0900

    feat(todos): 버튼 내부 텍스트를 span 태그로 변경하여 접근성 향상

--- src/routes/(main)/todos/+page.svelte
+++ src/routes/(main)/todos/+page.svelte
@@ -57,30 +57,29 @@
 		</button>
 	</form>
 	<div class="flex space-x-4 items-center-safe">
-			{#each page.data.weeklyTodos as dateItem}
-			<form action={page.url.pathname} >
-				<input type="hidden" name="startDate" value={dateItem.startDate} />
-				<input type="hidden" name="endDate" value={dateItem.endDate} />
-				<input type="hidden" name="hideStatusIds" value={page.url.searchParams.get('hideStatusIds')} />
-				<button
-					type="submit"
-					class={[
-					'btn relative h-16 w-16 flex flex-col',
-					dateItem.selected ? 'preset-filled-primary-500' : 'preset-filled-surface-50-950',
-					dateItem.empty ? '' : "before:content-[''] before:absolute before:top-1 before:right-1 before:w-2 before:h-2 before:bg-secondary-500 before:rounded-full",
-
-				]}
-				>
-					<div class="text-xs font-semibold">
-						{dateItem.dayName}
-					</div>
-					<div class="text-xs font-semibold">
-						{dateItem.day}
-					</div>
-				</button>
-			</form>
-			{/each}
-			</div>
+		{#each page.data.weeklyTodos as dateItem}
+		<form action={page.url.pathname} >
+			<input type="hidden" name="startDate" value={dateItem.startDate} />
+			<input type="hidden" name="endDate" value={dateItem.endDate} />
+			<input type="hidden" name="hideStatusIds" value={page.url.searchParams.get('hideStatusIds')} />
+			<button
+				type="submit"
+				class={[
+				'btn relative h-16 w-16 flex flex-col',
+				dateItem.selected ? 'preset-filled-primary-500' : 'preset-filled-surface-50-950',
+				dateItem.empty ? '' : "before:content-[''] before:absolute before:top-1 before:right-1 before:w-2 before:h-2 before:bg-secondary-500 before:rounded-full",
+			]}
+			>
+				<span class="text-xs font-semibold">
+					{dateItem.dayName}
+				</span>
+				<span class="text-xs font-semibold">
+					{dateItem.day}
+				</span>
+			</button>
+		</form>
+		{/each}
+	</div>
 	<form action={page.url.pathname} >
 		<input type="hidden" name="startDate" value={page.data.weeklyTodos.at(-1)?.startDate} />
 		<input type="hidden" name="endDate" value={page.data.weeklyTodos.at(-1)?.endDate} />
