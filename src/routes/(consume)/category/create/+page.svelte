ebcd1264c7dc312e0a8dead701f78c26ac02c7e5
김태희 <krb7korea@me.com>
2025-06-28 13:52:56 +0900

    Update ConsumeHeader to submit form by ID

---
+++ src/routes/(consume)/category/create/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { page } from '$app/state';
+</script>
+
+<form id="create-category-form" method="get" action={page.data.returnTo} class="p-4 space-y-4">
+  <input type="text" name="newCategory" placeholder="카테고리 이름" class="input w-full" required />
+</form>
